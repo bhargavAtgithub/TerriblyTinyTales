951f441700622397066fdd6af48ca003bbd0e281
Bhargav <bhargav999reddy@gmail.com>
2020-08-17 07:54:49 +0530

    client and server

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use((req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
 
