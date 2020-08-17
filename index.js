42f1f1fde105019f5edf8cf7b6a0c20d2e325110
Bhargav <bhargav999reddy@gmail.com>
2020-08-17 07:43:31 +0530

    client and server

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3001;
 
